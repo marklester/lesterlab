d90354abbff5a5954456f7ce1550635de8cae1b4
marklester <mlester@gmail.com>
2022-02-20 18:38:25 +0000

    remocve kpt folder`

--- kubernetes/kpt/transmission/protected/create-secret.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo -n "username:" 
-read username
-# Read Password
-echo -n "password:" 
-read -s password
-echo 
-kubectl create secret generic vpn-credentials -n transmission --from-literal=username="${username}" --from-literal=password="${password}"
