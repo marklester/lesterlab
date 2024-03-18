6e25fb89453bb0c3872ec24ec79c8641a7db24cb
marklester <mlester@gmail.com>
2024-03-18 12:40:36 +0000

    upgrade to rook 1.13.7 and ceph 18.2.2

--- kubernetes/crds/rook-ceph/get_resourses.sh
+++ kubernetes/crds/rook-ceph/get_resourses.sh
@@ -1,4 +1,4 @@
  
- VERSION="1.11.6"
+ VERSION="1.13.7"
  curl https://raw.githubusercontent.com/rook/rook/v$VERSION/deploy/examples/common.yaml -o common.yaml
  curl https://raw.githubusercontent.com/rook/rook/v$VERSION/deploy/examples/crds.yaml -o crds.yaml
