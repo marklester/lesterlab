214c49e6efc1b0d42528287db9e8e41ed1b769dc
marklester <mlester@gmail.com>
2024-09-08 13:48:36 +0000

    update to 18.4 and rook 14.10

--- kubernetes/crds/rook-ceph/get_resourses.sh
+++ kubernetes/crds/rook-ceph/get_resourses.sh
@@ -1,4 +1,5 @@
  
- VERSION="1.13.7"
- curl https://raw.githubusercontent.com/rook/rook/v$VERSION/deploy/examples/common.yaml -o common.yaml
- curl https://raw.githubusercontent.com/rook/rook/v$VERSION/deploy/examples/crds.yaml -o crds.yaml
+set -ex
+VERSION="1.14.10"
+curl https://raw.githubusercontent.com/rook/rook/v$VERSION/deploy/examples/common.yaml -o common.yaml
+curl https://raw.githubusercontent.com/rook/rook/v$VERSION/deploy/examples/crds.yaml -o crds.yaml
