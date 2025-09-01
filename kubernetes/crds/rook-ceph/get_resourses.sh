3423041d26f852480c1836000471624377d32db0
marklester <mlester@gmail.com>
2025-09-01 07:18:35 +0000

    * updated to rook 1.16.9

--- kubernetes/crds/rook-ceph/get_resourses.sh
+++ kubernetes/crds/rook-ceph/get_resourses.sh
@@ -1,5 +1,5 @@
  
 set -ex
-VERSION="1.15.6"
+VERSION="1.16.9"
 curl https://raw.githubusercontent.com/rook/rook/v$VERSION/deploy/examples/common.yaml -o common.yaml
 curl https://raw.githubusercontent.com/rook/rook/v$VERSION/deploy/examples/crds.yaml -o crds.yaml
