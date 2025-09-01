2eb764b655602d1ea18da1997583bbec9ed1f62f
marklester <mlester@gmail.com>
2025-09-01 19:01:57 +0000

    * update to 1.17 * update to ceph 19.2.3

--- kubernetes/crds/rook-ceph/get_resourses.sh
+++ kubernetes/crds/rook-ceph/get_resourses.sh
@@ -1,5 +1,5 @@
  
 set -ex
-VERSION="1.16.9"
+VERSION="1.17.8"
 curl https://raw.githubusercontent.com/rook/rook/v$VERSION/deploy/examples/common.yaml -o common.yaml
 curl https://raw.githubusercontent.com/rook/rook/v$VERSION/deploy/examples/crds.yaml -o crds.yaml
