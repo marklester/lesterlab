e0e0ba27510dc9c726a3ab115ffbd7b26657efdd
Mark <mlester@gmail.com>
2026-09-04 02:33:08 -0400

    remove more unused resources

--- kubernetes/inactive/amcrest2mqtt/protected/create-secret.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo -n "username:" 
-read username
-# Read Password
-echo -n "password:" 
-read -s password
-echo 
-kubectl create secret generic camera-credentials -n mqtt --from-literal=username="${username}" --from-literal=password="${password}"
