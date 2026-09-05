9e3c8c059a74ccf309d536e4b855455f8bd94cf4
Mark <mlester@gmail.com>
2026-09-04 21:45:27 -0400

    update rook-ceph to 1.19.11

--- kubernetes/crds/rook-ceph/get_resourses.sh
+++ kubernetes/crds/rook-ceph/get_resourses.sh
@@ -1,7 +1,8 @@
  
 set -ex
-VERSION="1.19.7"
+VERSION="1.19.11"
 curl https://raw.githubusercontent.com/rook/rook/v$VERSION/deploy/examples/common.yaml -o common.yaml
 curl https://raw.githubusercontent.com/rook/rook/v$VERSION/deploy/examples/crds.yaml -o crds.yaml
 curl https://raw.githubusercontent.com/rook/rook/v$VERSION/deploy/examples/csi-operator.yaml -o csi-operator.yaml
 curl https://raw.githubusercontent.com/rook/rook/v$VERSION/deploy/examples/monitoring/rbac.yaml -o rbac.yaml
+curl https://raw.githubusercontent.com/rook/rook/v$VERSION/deploy/examples/toolbox.yaml -o toolbox.yaml
